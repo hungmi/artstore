b925dfcca66214ac7114f82d6b691749a612a263
hungmi <hungmihungmi@gmail.com>
2015-11-30 20:13:35 +0800

    add dev hashes to production in pay2go.rb

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -6,10 +6,10 @@ Pay2go.setup do |pay2go|
     pay2go.hash_iv     = "QoksBABApax6VtKI"
     pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
   else
-    pay2go.merchant_id = "1234567"  # 放正式站的 key
+    pay2go.merchant_id = "11109109"  # 放正式站的 key
 
-    pay2go.hash_key    = "xxxxxxxx"
-    pay2go.hash_iv     = "xxxxxxxx"
+    pay2go.hash_key    = "4koMBcf9ZOzWGLWEMHhen2w0ZKQwtw0y"
+    pay2go.hash_iv     = "QoksBABApax6VtKI"
     pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
   end
 end
